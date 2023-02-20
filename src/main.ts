477932a2e892bb57c2554db4d91ced6799eccba6
Zach Kusz <z.s.kusz@gmail.com>
2023-02-19 19:50:57 -0600

    chore: tree shake vuetify imports

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import 'vuetify/styles';
 import '@mdi/font/css/materialdesignicons.css';
 import './style.css';
 import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import App from './App.vue';
 
@@ -22,8 +20,6 @@ const router = createRouter({
 });
 
 const vuetify = createVuetify({
-  components,
-  directives,
   icons: {
     aliases,
     defaultSet: 'mdi',
