c38ced9b08e6dfc9999161ec407e327255aa51af
Zach Kusz <z.s.kusz@gmail.com>
2023-02-21 20:58:13 -0600

    fix: use combobox to reduce clicks for custom data

--- src/components/item/ItemEdit.vue
+++ src/components/item/ItemEdit.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import type { Ref } from 'vue'
 import Item from '@/types/item';
 import lootJSON from '@/data/loot';
 
@@ -13,7 +12,6 @@ const item = props.item;
 
 const name = ref(item.name);
 const count = ref(item.count);
-const inputType: Ref<'autocomplete' | 'input'> = ref('autocomplete');
 
 const lootCats = JSON.parse(lootJSON);
 const loot = [
@@ -41,22 +39,15 @@ const save = () => {
 const remove = () => {
     emit('removeItem', item.id);
 };
-const toggleInputType = () => {
-    inputType.value = inputType.value === 'autocomplete' ? 'input' : 'autocomplete';
-};
-const noDataText = 'Click the typewriter to add custom value';
 </script>
 
 <template>
     <form class="flex" @submit.prevent="save">
-        <v-autocomplete v-if="inputType === 'autocomplete'" :no-data-text="noDataText"
+        <v-combobox
             hide-details class="mr-1 name" v-model="name" :items="loot" density="compact" spellcheck="false">
-        </v-autocomplete>
-        <v-text-field v-else hide-details class="mr-1 name" v-model="name" density="compact"></v-text-field>
-
+        </v-combobox>
         <v-text-field  hide-details class="mx-1 count" label="Count" v-model="count" type="number" density="compact"></v-text-field>
         <v-btn class="mx-1" icon="mdi-delete-circle-outline" @click.prevent="remove"  color="error"></v-btn>
-        <v-btn class="mx-1" icon="mdi-typewriter" @click.prevent="toggleInputType" color="secondary"></v-btn>
         <v-btn type="submit" class="ml-1" icon="mdi-content-save-outline" color="primary"></v-btn>
     </form>
 </template>
