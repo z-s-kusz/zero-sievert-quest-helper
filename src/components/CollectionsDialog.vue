f79fca5971a53098e4c79694b90ba7793317d413
Zach Kusz <zack.kusz@capitollien.com>
2024-01-03 14:53:35 -0600

    fix dialog size and actions position

--- src/components/CollectionsDialog.vue
+++ src/components/CollectionsDialog.vue
@@ -4,7 +4,7 @@ import { useGetDBCollections } from '@/hooks/useGetDBCollections';
 
 const emit = defineEmits(['addCollection']);
 const selection = ref<any>({});
-const { collections, error } = useGetDBCollections();
+const { collections, error, isLoading } = useGetDBCollections();
 
 const confirmSelection = (dialogActiveControl: any) => {
     emit('addCollection', selection.value);
@@ -19,36 +19,40 @@ const setSelection = (quest: any) => {
 </script>
 
 <template>
-    <v-dialog width="500">
+    <v-dialog width="auto" max-width="1080" scrollable>
         <!-- no idea how these undeclared 'props' and 'isActive' work in the dialog, but they work -->
         <template v-slot:activator="{ props }">
             <v-btn v-bind="props" color="secondary">Add Quest!</v-btn>
         </template>
 
         <template v-slot:default="{ isActive }">
-            <v-card title="Select Quest or Module">
-                <v-table class="">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Items/Tasks</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="quest in collections" :key="quest.id" @click="setSelection(quest)"
-                            :class="selection.id === quest.id ? 'selected' : ''">
-                            <td>{{ quest.name }}</td>
-                            <td>
-                                <ul>
-                                    <li v-for="item in quest.items" :key="item.id">{{ item.name }}: {{ item.count }}</li>
-                                </ul>
-                            </td>
-                        </tr>
-                    </tbody>
-                </v-table>
-                <v-card-text v-if="error">{{ error }}</v-card-text>
+            <v-card title="Select Quest or Module" :loading="isLoading">
+                <v-card-text>
+                    <span v-if="error">{{ error }}</span>
 
-                <v-card-actions>
+                    <v-table class="">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Items/Tasks</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="quest in collections" :key="quest.id" @click="setSelection(quest)"
+                                :class="selection.id === quest.id ? 'selected' : ''">
+                                <td>{{ quest.name }}</td>
+                                <td>
+                                    <ul>
+                                        <li v-for="item in quest.items" :key="item.id">{{ item.name }}: {{ item.count }}
+                                        </li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </v-table>
+                </v-card-text>
+
+                <v-card-actions fixed>
                     <v-btn @click="isActive.value = false">Cancel</v-btn>
                     <v-btn color="primary" variant="tonal" @click="confirmSelection(isActive)"
                         :disabled="!selection.id">Confirm</v-btn>
